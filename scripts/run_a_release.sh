c8890d8b58fc4427212cdbbcca5a424f8152fff9
klawik-j <kklawik345@gmail.com>
2024-06-23 18:12:32 +0200

    Fix prod.dockerfile missing arg.

--- scripts/run_a_release.sh
+++ scripts/run_a_release.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-scripts/build_package.sh
-scripts/upload_package.sh
+# scripts/build_package.sh
+# scripts/upload_package.sh
 
 # Extract version string
 export PACKAGE_VERSION=$(grep "__version__ =" src/hemse/version.py | awk -F'=' '{print $3}' | tr -d " '" | tr '+' '-')
+export PACKAGE_VERSION='1.0.1'
 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 scripts/build_prod_image.sh

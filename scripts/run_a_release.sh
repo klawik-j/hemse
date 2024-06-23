f6a0ed7cd58c7acc3939fc15783341d362f5f67c
klawik-j <kklawik345@gmail.com>
2024-06-23 17:56:38 +0200

    Fix run_a_relese.sh

--- scripts/run_a_release.sh
+++ scripts/run_a_release.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-# scripts/build_package.sh
-# scripts/upload_package.sh
+scripts/build_package.sh
+scripts/upload_package.sh
 
 # Extract version string
 export PACKAGE_VERSION=$(grep "__version__ =" src/hemse/version.py | awk -F'=' '{print $3}' | tr -d " '" | tr '+' '-')
-export PACKAGE_VERSION='0.1.0'
 
-# echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 scripts/build_prod_image.sh
-# scripts/upload_prod_image.sh
-# docker logout
+scripts/upload_prod_image.sh
+docker logout
